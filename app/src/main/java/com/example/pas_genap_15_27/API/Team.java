f6a7c46f003383bca6723b9f6d31ee0e02fa8bed
nmdntaa <144686774+nmdntaa@users.noreply.github.com>
2025-05-22 11:04:09 +0700

    feat [NM] country

--- app/src/main/java/com/example/pas_genap_15_27/API/Team.java
+++ app/src/main/java/com/example/pas_genap_15_27/API/Team.java
@@ -1,4 +1,20 @@
 package com.example.pas_genap_15_27.API;
 
 public class Team {
+
+    private String name_en;
+    private String flag_url_32;
+
+    public Team(String name_en, String flag_url_32){
+        this.name_en=name_en;
+        this.flag_url_32=flag_url_32;
+    }
+
+    public String getFlag_url_32() {
+        return flag_url_32;
+    }
+
+    public String getName_en() {
+        return name_en;
+    }
 }
---
+++ app/src/main/java/com/example/pas_genap_15_27/API/TeamResponse.java
@@ -0,0 +1,14 @@
+package com.example.pas_genap_15_27.API;
+
+import com.example.pas_genap_15_27.CountriesFragment;
+
+import java.util.List;
+
+public class TeamResponse {
+
+    private List<Country> Country;
+
+    public List<CountriesFragment> getCountries() {
+        return countries;
+    }
+}
